912eee92e906386f34929779815a8f072297f488
RoshanDileep <118088610+RoshanDileep@users.noreply.github.com>
2023-07-26 15:31:03 -0700

    Add previous work html file

---
+++ script.js
@@ -0,0 +1,10 @@
+const pythonExp = document.querySelector('.pythonexp h2');
+const h1 = document.querySelector('h1');
+const button = document.querySelector('.click-button');
+
+h1.addEventListener('animationend', () => {
+    pythonExp.style.opacity = '1';
+    pythonExp.classList.add('fadeIn');
+    button.style.display = 'block';
+    button.classList.add('fadeIn');
+});
